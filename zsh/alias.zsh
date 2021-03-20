b9b4d4195715ba4193966b4090122a82cb3aaf1d
Julien Calixte <julienc@bam.tech>
2021-03-20 13:24:08 +0100

    :sparkles: (alias) update aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -16,20 +16,17 @@ alias gcz="git cz"
 alias lab="cd ~/lab"
 alias labcli="cd ~/lab-cli"
 alias jc="cd ~/jclab"
-alias rnios="react-native run-ios"
-alias rnand="react-native run-android"
+alias note="cd ~/jcnote"
 alias podinstall="cd ios && bundle exec pod install && cd .."
 alias deployand="bundle exec fastlane android deploy --env=staging"
 alias deployios="bundle exec fastlane ios deploy --env=staging"
-alias simulator='open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app'
 alias emulatepixel="~/Library/Android/sdk/emulator/emulator -avd Pixel_3_XL_API_29"
 alias emulatenexus="~/Library/Android/sdk/emulator/emulator -avd Nexus_5_API_28"
 alias resetiphone="sudo killall -9 com.apple.CoreSimulator.CoreSimulatorService"
 alias tct="tiny-care-terminal"
-alias liveand="scrcpy"
+alias liveandroid="scrcpy"
 alias sshpi="ssh pi@192.168.1.36"
 alias ide="tmux split-window -h -p 30 && tmux split-window -v -p 66"
-alias vueconfig="cp -r ~/vueconfig/. ."
 alias chdot="code ~/.dotfiles"
 
 # yarn commands
@@ -37,15 +34,15 @@ alias ys="yarn serve"
 
 # side project aliases
 alias bp="cd ~/jclab/bons-programmeurs"
+alias jcln="cd ~/jclab/lite-note"
 
 # Dotnet Core aliases
 alias efdbreset="rm -rf Migrations && dotnet ef database drop --force && dotnet ef migrations add Initial && dotnet ef database update"
 
 # BAM Project aliases
-alias fwe="cd ~/bddf_app_pri/apps/FWE"
-alias fwe-cov="open coverage/lcov-report/index.html"
 alias col-mob="cd ~/lab/chantier360/apps/front/mobile"
 alias col-ser="cd ~/lab/chantier360/apps/server"
-alias ywf="yarn workspace fwe"
+alias col-doc="cd ~/lab/chantier360/apps/docs"
 
+# Weather
 alias wtr='curl "wttr.in/Paris?lang=fr"'
