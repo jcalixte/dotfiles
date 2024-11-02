47807a6da393bd54a6a49cd9e1aff4882353b927
Julien Calixte <julienc@bam.tech>
2024-11-02 12:09:15 +0100

    add oh my posh to macos

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -5,3 +5,6 @@ fi
 
 # source /Users/julien/.kepler/kntools/environment-setup-sdk.sh
 # source /Users/julien/.kepler/kntools/sdk/0.10.4/environment-setup-sdk.sh
+
+# From https://raw.githubusercontent.com/dreamsofautonomy/zen-omp/main/zen.toml
+eval "$(oh-my-posh init zsh --config $HOME/.config/ohmyposh/zen.toml)"
