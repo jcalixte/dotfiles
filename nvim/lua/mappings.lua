c429d36c22dc921eb130b786b09e0a0d28b202dd
Julien Calixte <julienc@bam.tech>
2024-11-11 15:00:18 +0100

    format on save and cool mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -6,4 +6,16 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("n", "<c-z>", "<nop>")
+map("i", "jj", "<Esc>", { desc = "Exit insert mode with jj", noremap = true, silent = true })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map('n', '<C-M-t>', ':lua CloseOtherBuffers()<CR>', { noremap = true, silent = true })
+
+function CloseOtherBuffers()
+  local current = vim.fn.bufnr('%')
+  for _, bufnr in ipairs(vim.fn.getbufinfo({ buflisted = 1 })) do
+    if bufnr.bufnr ~= current then
+      vim.api.nvim_buf_delete(bufnr.bufnr, { force = true })
+    end
+  end
+end
